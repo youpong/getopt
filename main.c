6e7ce0f476b1c8f10480a52aee081791e540cd0a
NAKAJIMA Yusaku <yupong7@gmail.com>
2022-11-22 01:07:54 +0900

    wip

--- main.c
+++ main.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+    char *OPT = getenv("OPT"); // "LONG" | "SHORT"
+
+    void (*manual_test)(int, char *const *) = manual_test_getopt;
+    if (OPT && !strcmp(OPT, "LONG")) {
+        manual_test = manual_test_getopt_long;
+    }
+
     if (argc != 1) {
-        // manual_test_getopt(argc, argv);
-        manual_test_getopt_long(argc, argv);
+        manual_test(argc, argv);
         return 0;
     }
 
-    if (testsuite_getopt() + testsuite_getopt_long() == 0) {
-        printf("All tests passed!\n");
-    } else {
+    if (testsuite_getopt() || testsuite_getopt_long()) {
         printf("Failed to pass all tests.\n");
         return 1;
     }
+
+    printf("All tests passed!\n");
+    return 0;
 }
